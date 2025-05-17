b3cd4421915ed41a5d213479b74715dd67a5147e
Lucas Arruda <lucasrarruda@gmail.com>
2025-05-16 21:54:14 -0300

    Added a new detection model

--- Phase4/face_detect_strategy.py
+++ Phase4/face_detect_strategy.py
@@ -1,17 +1,39 @@
 import cv2
+import numpy as np
 from player_strategy import PlayerStrategy
 
 class FaceDetectStrategy(PlayerStrategy):
     def __init__(self):
-        self.face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+        self.face_dnn = cv2.dnn.readNetFromCaffe(
+            'Phase4/models/deploy.prototxt.txt',
+            'Phase4/models/res10_300x300_ssd_iter_140000.caffemodel'
+        )
 
-    def process_frame(self, frame, _):
+    def process_frame(self, frame, video_capture):
+        self.trackers = []
         faces = self.detect_faces(frame)
         return self.draw_faces_rectangle(faces, frame)
-
+    
     def detect_faces(self, frame):
-        gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = self.face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(60, 60))
+        blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
+                                    (300, 300), (104.0, 177.0, 123.0))
+        self.face_dnn.setInput(blob)
+        detections = self.face_dnn.forward()
+        return self.get_faces(detections, frame)
+    
+    def get_faces(self, detections, frame):
+        h, w = frame.shape[:2]
+        faces = []
+        for i in range(detections.shape[2]):
+            confidence = detections[0, 0, i, 2]
+            if confidence > 0.6:
+                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                (startX, startY, endX, endY) = box.astype("int")
+                x = startX
+                y = startY
+                width = endX - startX
+                height = endY - startY
+                faces.append((x, y, width, height))
         return faces
 
     def draw_faces_rectangle(self, faces, frame):
--- Phase4/main.py
+++ Phase4/main.py
@@ -4,18 +4,18 @@ from face_recognition_strategy  import FaceRecognitionStrategy
 from player import Player
 
 if __name__ == "__main__":
-    video_path = 'Phase4/videoplayback.mp4'
+    video_path = 'Phase4/sample.mp4'
 
     face_detect_strategy = FaceDetectStrategy()
     player = Player(video_path, face_detect_strategy, window_title="Face Detection")
     player.play()
 
-    face_recognition_strategy = FaceRecognitionStrategy()
-    player.set_strategy(face_recognition_strategy)
-    player.window_title = "Face Recognition"
-    player.play()
+    # face_recognition_strategy = FaceRecognitionStrategy()
+    # player.set_strategy(face_recognition_strategy)
+    # player.window_title = "Face Recognition"
+    # player.play()
 
-    face_emotion_strategy = FaceEmotionStrategy()
-    player.set_strategy(face_emotion_strategy)
-    player.window_title = "Face Emotion Recognition"
-    player.play()
+    # face_emotion_strategy = FaceEmotionStrategy()
+    # player.set_strategy(face_emotion_strategy)
+    # player.window_title = "Face Emotion Recognition"
+    # player.play()
